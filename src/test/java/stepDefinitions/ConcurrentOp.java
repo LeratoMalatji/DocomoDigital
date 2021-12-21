7e59cc5efd71489e18b1544b3b17c8b236c299f3
lerato <leratomalatji@ymail.com>
2021-12-21 13:10:14 +0200

    test cases added to test if one of the threads returns status 201

--- src/test/java/stepDefinitions/ConcurrentOp.java
+++ src/test/java/stepDefinitions/ConcurrentOp.java
@@ -8,6 +8,7 @@ import io.restassured.config.HttpClientConfig;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Assert;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -20,45 +21,57 @@ public class ConcurrentOp {
 
     RequestSpecification request;
     Response response;
-    private final  static Logger logr = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    private final static Logger logr = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    private int status;
+    private int actualStatus;
 
     @Given("^User sets the base API to request this (.+)$")
     public void user_sets_the_base_api_to_request_this(String url) throws Throwable {
-        request= given().baseUri(url).config(RestAssured.config().httpClient(HttpClientConfig.httpClientConfig().dontReuseHttpClientInstance()));
-        logr.log(Level.INFO,"Connection opened with the base URL");
+        request = given().baseUri(url).config(RestAssured.config().httpClient(HttpClientConfig.httpClientConfig().dontReuseHttpClientInstance()));
+        logr.log(Level.INFO, "Connection opened with the base URL");
     }
 
     @When("^User sends the request to this (.+) (.+) with UUid that is valid concurrently$")
     public void user_sends_the_request_to_this_with_uuid_that_is_valid_concurrently(String url, String endpoint) throws Throwable {
 
-        response= request.post(endpoint).then().extract().response();
-
+        response = request.post(endpoint).then().extract().response();
         ExecutorService executorService = Executors.newFixedThreadPool(10);// Java to allocate 10 threads
 
         //do while loop that will create 10 of the same threads
-        do{
+        do {
             executorService.execute(new Runnable() {
                 @Override
-                public void run(){
+                public void run() {
                     try {
-                        request= given().baseUri(url);
-                        response= request.post(endpoint).then().extract().response();
-                        logr.log(Level.INFO,"Request sent multi threaded");
+                        request = given().baseUri(url);
+                        response = request.post(endpoint).then().extract().response();
+
+                        if (response.getStatusCode() == 201)//
+                        {
+                            status = response.getStatusCode();
+                            logr.log(Level.INFO, "One of the threads come back with status :" + response.getStatusCode());
+                        } else if (response.getStatusCode() == 423) {
+                            actualStatus = response.getStatusCode();
+                        }
+
+                        logr.log(Level.INFO, "Request sent multi threaded");
                     } catch (Exception e) {
-                        logr.log(Level.SEVERE,e.getMessage());
+                        logr.log(Level.SEVERE, e.getMessage());
                     }
                 }
             });
-        }while(response.getStatusCode()!=423);
+        } while (response.getStatusCode() != 423);
 
         // Wait for all the threads to gracefully shutdown
         try {
-            executorService.awaitTermination(200, TimeUnit.MILLISECONDS);
-            logr.log(Level.INFO,"Threads shut down");
+
+
+            executorService.awaitTermination(2000, TimeUnit.MILLISECONDS);
+            logr.log(Level.INFO, "Threads shut down");
         } catch (InterruptedException ie) {
             System.out.println("Shutdown interrupted. Will try to shutdown again.");
             executorService.shutdownNow();
-            logr.log(Level.SEVERE,ie.getMessage());
+            logr.log(Level.SEVERE, ie.getMessage());
         }
 
     }
@@ -66,9 +79,18 @@ public class ConcurrentOp {
 
     @Then("^User should be presented with  (.+) as response on one of the transactions$")
     public void user_should_be_presented_with_as_response_on_one_of_the_transactions(String statuscode) throws Throwable {
-        int code =new Integer(statuscode);
-        Assert.assertEquals(code,response.getStatusCode());
-        logr.log(Level.INFO,"Assertion done");
+        int code = new Integer(statuscode);
+        Assert.assertEquals(code, actualStatus);
+        logr.log(Level.INFO, "Assertion done");
     }
+
+    @Then("^One of the request that was sent concurrently should return  the given status code (.+)$")
+    public void one_of_the_request_that_was_sent_concurrently_should_return_the_given_status_code(String codestatus) throws Throwable {
+
+        int code = new Integer(codestatus);
+        Assert.assertEquals(code, status);
+        logr.log(Level.INFO, "Assertion done");
+    }
+
 
 }
--- src/test/resources/features/ConcurrentOparation.feature
+++ src/test/resources/features/ConcurrentOparation.feature
@@ -4,9 +4,10 @@ Feature: Payment gateway concurrent
 
     Given  User sets the base API to request this <URL>
     When User sends the request to this <URL> <endpoint> with UUid that is valid concurrently
+    Then  One of the request that was sent concurrently should return  the given status code <codestatus>
     Then User should be presented with  <statuscode> as response on one of the transactions
 
 
     Examples:
-      | URL                   | endpoint                                               | statuscode |
-      | http://localhost:8088 | /operations/501b6daa-d586-4afa-b0c5-479b0abd89cd/refund | 423        |
+      | URL                   | endpoint                                                | statuscode | codestatus |
+      | http://localhost:8088 | /operations/501b6daa-d586-4afa-b0c5-479b0abd89cd/refund | 423        | 201        |
